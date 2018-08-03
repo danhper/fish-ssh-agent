e09c21501c20730634ab80d6bc9329335eabe065
kyb <i.kyb@ya.ru>
2018-08-03 08:25:45 +0300

    All in one file.

--- functions/__ssh_agent_start.fish
+++
@@ -1,6 +0,0 @@
-function __ssh_agent_start -d "start a new ssh agent"
-   ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
-   chmod 600 $SSH_ENV
-   source $SSH_ENV > /dev/null
-   true  # suppress errors from setenv, i.e. set -gx
-end
--- functions/fish_ssh_agent.fish
+++ functions/fish_ssh_agent.fish
@@ -1,3 +1,26 @@
+function __ssh_agent_is_started -d "check if ssh agent is already started"
+   if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
+      source $SSH_ENV > /dev/null
+   end
+
+   if test -z "$SSH_AGENT_PID"
+      return 1
+   end
+
+   ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
+   #pgrep ssh-agent
+   return $status
+end
+
+
+function __ssh_agent_start -d "start a new ssh agent"
+   ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+   chmod 600 $SSH_ENV
+   source $SSH_ENV > /dev/null
+   true  # suppress errors from setenv, i.e. set -gx
+end
+
+
 function fish_ssh_agent --description "Start ssh-agent if not started yet, or uses already started ssh-agent."
    if test -z "$SSH_ENV"
       set -xg SSH_ENV $HOME/.ssh/environment
