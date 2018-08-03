55a2ef16d4a3f77631b981f31cf8ed0c44f8c036
kyb <i.kyb@ya.ru>
2018-08-03 08:22:42 +0300

    simple install

--- install.fish
+++ install.fish
@@ -1,3 +1,5 @@
 
-source functions/*
-funcsave __ssh_agent_is_started  __ssh_agent_start  fish-ssh-agent
+#source functions/*
+#funcsave __ssh_agent_is_started  __ssh_agent_start  fish-ssh-agent
+
+cp functions/fish_ssh_agent.fish ~/.config/fish/functions/
